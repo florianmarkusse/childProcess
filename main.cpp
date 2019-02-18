81371c0f0c9266fc473314180e2b7d1260a9c244
florianmarkusse <florianmarkusse@hotmail.com>
2019-02-18 13:30:58 -0800

    minor changes to child process

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <unistd.h>
+#include <mqueue.h>
+#include <fcntl.h>           
+#include <sys/stat.h>
+#include <cstdlib>
 
 
 int main(int argc, char * argv[]) {
+
+    for (int i = 0; i < argc; ++i) {
+
+		std::cout << argv[i] << "\n";
+
+    }
+
+    mqd_t messageQueueDescriptorClient;
+
+    if ((messageQueueDescriptorClient = mq_open(argv[1], O_RDONLY)) == -1) {
+        perror ("cant open server from client");
+        exit (1);
+    }
+
+    char outBuffer[512];
+
+    if ((mq_receive(messageQueueDescriptorClient, outBuffer, 512, NULL)) == -1) {
+        perror ("cant receive from server");
+        exit (1);
+    }
+
+    std::cout << "reveived this:\n";
+    std::cout << outBuffer << "\n";
     
     std::cout << "hello from the other executable\n";
     std::cin.get();

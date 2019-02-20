3f7b382b6fcad95d9247d99dc5e6d18dc7f46afd
florianmarkusse <florianmarkusse@hotmail.com>
2019-02-20 13:23:15 -0800

    Added termination and time

--- main.cpp
+++ main.cpp
@@ -8,30 +8,44 @@
 #include <cstdlib>
 #include <array>
 
+#define MESSAGE_SIZE 4096
+#define EXIT_CHAR 1
+
 
 int main(int argc, char * argv[]) {
 
-    for (int i = 0; i < argc; ++i) {
-
-		std::cout << argv[i] << "\n";
-
-    }
+	if (argc != 2) {
+		std::cerr 
+			<< "not enough arguments\n"
+			<< "call syntax: main [Messagequeue name]"
+			<< std::endl;
+		return EXIT_FAILURE;
+	}
 
 	logger::MessageQueue read = logger::MessageQueue::open(argv[1], logger::MessageQueueMode::read);
 
-	std::array<char, 4096> buffer = 
-		std::array<char, 4096>{};
+	std::array<char, MESSAGE_SIZE> buffer = 
+		std::array<char, MESSAGE_SIZE>{};
 
-	unsigned int bytesRead = read.read( logger::Buffer {
-		buffer.data(),
-		buffer.size()
-	});
+	bool isQueueOpen = true;
 
-	std::cout << "I read " << bytesRead << " bytes\n";
-    
+	while (isQueueOpen) {
 
-    std::cout << "reveived this:\n";
-    std::cout << buffer.data() << "\n";
-    
-    std::cin.get();
+		unsigned int bytesRead = read.read( logger::Buffer {
+			buffer.data(),
+			buffer.size()
+		});
+		
+		for (unsigned int i = 0; i < bytesRead; ++i) {
+			if (static_cast<unsigned int> (buffer[i]) == EXIT_CHAR) {
+				isQueueOpen = false;
+				std::cout << "set to false\n";
+			} else {
+				std::cout << buffer[i];
+			}
+		}
+
+	}
+
+	return EXIT_SUCCESS;
 }

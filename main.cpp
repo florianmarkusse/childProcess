0efca62e038d0a7ce6839f8ae4c63e65c1240e69
florianmarkusse <florianmarkusse@hotmail.com>
2019-02-28 09:46:38 -0800

    added colors for linux OS

--- main.cpp
+++ main.cpp
@@ -9,7 +9,17 @@
 #include <array>
 
 #define MESSAGE_SIZE 4096
+
 #define EXIT_CHAR 1
+#define INFO_CHAR 2
+#define WARN_CHAR 3
+#define ERROR_CHAR 4
+#define SUCCESS_CHAR 5
+
+#define NORMAL  "\x1B[0m" 
+#define RED  "\x1B[31m"
+#define GREEN  "\x1B[32m"
+#define YELLOW  "\x1B[33m"
 
 
 int main(int argc, char * argv[]) {
@@ -37,12 +47,35 @@ int main(int argc, char * argv[]) {
 		});
 		
 		for (unsigned int i = 0; i < bytesRead; ++i) {
-			if (static_cast<unsigned int> (buffer[i]) == EXIT_CHAR) {
-				isQueueOpen = false;
-				std::cout << "set to false\n";
-			} else {
-				std::cout << buffer[i];
+
+			switch (static_cast<unsigned int> (buffer[i]))
+			{
+				case EXIT_CHAR: {
+					isQueueOpen = false;
+				}
+					break;
+				case INFO_CHAR: {
+					printf("%s", NORMAL);
+				}
+					break;
+				case WARN_CHAR: {
+					printf("%s", YELLOW);
+				}
+					break;
+				case ERROR_CHAR: {
+					printf("%s", RED);
+				}
+					break;
+				case SUCCESS_CHAR: {
+					printf("%s", GREEN);
+				}
+					break;
+				default: {
+					std::cout << buffer[i];
+				}
+					break;
 			}
+
 		}
 
 	}
